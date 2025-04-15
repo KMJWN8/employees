8970436ccd87f7fe1343fbd904be3d1f846bcf1a
KMJWN8 <pilatp618@gmail.com>
2025-04-15 19:17:55 +0300

    5.1

--- frontend/test_project/src/components/EmployeeList.vue
+++ frontend/test_project/src/components/EmployeeList.vue
@@ -41,7 +41,7 @@
       </button>
 
     </div>
-    <div v-else class="text-black text-xl m-6"> Выберите подразделение </div>
+    <div v-else-if="!selectedNode" class="text-black text-xl m-6"> Выберите подразделение </div>
 
     <div class="grid grid-cols-1 md:grid-cols-2 gap-2">
       <!-- Карточки сотрудников -->
